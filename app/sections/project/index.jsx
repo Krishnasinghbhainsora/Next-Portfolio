32a1641001c2555499a65c2efa7b4d06da01e1a1
Krishnasinghbhainsora <krishnabhainsora@gmail.com>
2024-10-21 13:57:16 +0530

    Krishna

--- app/sections/project/index.jsx
+++ app/sections/project/index.jsx
@@ -17,9 +17,9 @@ const projects = [
 		description:
 			"Frontend Technologies:- React: Utilized for building dynamic and responsive user interfaces. Bootstrap: Employed for styling and ensuring a consistent look and feel across different devices.Backend Technologies:- Express: Used to develop server-side logic andmanage routes for handling API requests. Database Management:- MongoDB: Implemented for storing and retrieving application data with a NoSQL database. Deployment:- Hostinger: Deployed applications to a live server using Hostinger for hosting and managing web services.",
 		images: [
-			"https://res.cloudinary.com/dsq1kewrn/image/upload/v1729329965/uploads/pr9syzcjj7t3oy0puddy.png"
+			"https://res.cloudinary.com/dq7ttfnfb/image/upload/v1729493918/Screenshot_from_2024-10-19_14-41-56_sgpkxg.png"
 		],
-		liveUrl: "https://example.com/live1",
+		liveUrl: "https://holyvrindavantour.net/",
 		repoUrl: "https://github.com/example/repo1",
 		stack: ["React", "Express", "Node.js"]
 	},
@@ -27,9 +27,21 @@ const projects = [
 		id: 2,
 		title: "Project 2",
 		description:
-			"Developed a comprehensive website for ModaTemps, a fashion and lifestyle brand, with a focus on responsiveness, interactivity, and user engagement. Tech Stack: React, Bootstrap, CSS, React Router, React Icons, React Toast, React Bootstrap, Node.js, Express, MongoDB. Forms and Authentication: Developed sign-in and sign-up forms with OTP veriﬁcation, connected to backend APIs for secure authentication. Backend Integration: Built backend with Node.js, Express, and MongoDB",
+		"Developed a comprehensive website for ModaTemps, a fashion and lifestyle brand, with a focus on responsiveness, interactivity, and user engagement. Tech Stack: React, Bootstrap, CSS, React Router, React Icons, React Toast, React Bootstrap, Node.js, Express, MongoDB. Forms and Authentication: Developed sign-in and sign-up forms with OTP veriﬁcation, connected to backend APIs for secure authentication. Backend Integration: Built backend with Node.js, Express, and MongoDB",
 		images: [
-			"https://res.cloudinary.com/dsq1kewrn/image/upload/v1729329966/uploads/w60sf6opbfhscisevljq.png"
+			"https://res.cloudinary.com/dq7ttfnfb/image/upload/v1729493918/Screenshot_from_2024-10-19_14-41-56_sgpkxg.png"
+		],
+		liveUrl: "https://www.modatemps.com/",
+		repoUrl: "https://github.com/example/repo2",
+		stack: ["React", "Express", "Node.js"]
+	},
+	{
+		id: 4,
+		title: "Project 4",
+		description:
+			"Currently building web applications using React , Node.js , Express , and Bootstrap . Actively developing RESTful APIs using Express for smooth data ﬂow between frontend and backend. Implemented ﬁlter functionality for users to search properties by location, price, and type. Managing dynamic state with React hooks and improving frontend functionality.",
+		images: [
+			"https://res.cloudinary.com/dq7ttfnfb/image/upload/v1729493954/zwolf_fno0b2.png"
 		],
 		liveUrl: "https://example.com/live2",
 		repoUrl: "https://github.com/example/repo2",
@@ -41,24 +53,12 @@ const projects = [
 		description:
 			"I successfully designed and developed the official Zwolf Consultancy Services website, significantly enhancing the company’s online presence and user engagement. The website features a modern and intuitive interface, ensuring seamless navigation and an enjoyable user experience. Utilizing responsive design principles, I ensured optimal functionality across various devices, from desktops to mobile phones. The integration of vibrant visuals and informative content showcases the company’s services effectively, attracting potential clients and improving overall brand visibility. Additionally, I implemented SEO best practices to boost search engine rankings, contributing to increased traffic and visibility for the consultancy in a competitive market.",
 		images: [
-			"https://res.cloudinary.com/dsq1kewrn/image/upload/v1729329967/uploads/zgvix1od1agdphslrxld.png"
+			"https://res.cloudinary.com/dq7ttfnfb/image/upload/v1729493953/ZCS_u1p8tk.png"
 		],
-		liveUrl: "https://example.com/live1",
+		liveUrl: "https://www.zwolfconsultancyservice.com/",
 		repoUrl: "https://github.com/example/repo1",
 		stack: ["React"]
 	},
-	{
-		id: 4,
-		title: "Project 4",
-		description:
-			"Currently building web applications using React , Node.js , Express , and Bootstrap . Actively developing RESTful APIs using Express for smooth data ﬂow between frontend and backend. Implemented ﬁlter functionality for users to search properties by location, price, and type. Managing dynamic state with React hooks and improving frontend functionality.",
-		images: [
-			"https://res.cloudinary.com/dsq1kewrn/image/upload/v1729329970/uploads/nmz0uutcive4uhnakx4t.png"
-		],
-		liveUrl: "https://example.com/live2",
-		repoUrl: "https://github.com/example/repo2",
-		stack: ["React", "Express", "Node.js"]
-	}
 ];
 
 export function ProjectsSection() {
